3fdc20762fd5b5c813ad28ee70a19fc00623d70a
Kazuki Kyakuno <kyakuno@axell.co.jp>
2023-06-14 22:06:46 +0900

    Added base code

---
+++ sentence_transformers/sentence_transformers.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+MODEL="sentence-transformers-japanese"
+FILE1="paraphrase-multilingual-mpnet-base-v2.onnx"
+FILE2="paraphrase-multilingual-mpnet-base-v2.onnx.prototxt"
+FILE3="sentencepiece.bpe.model"
+
+#download
+if [ ! "$1" = "-h" ] && [ ! "$1" = "--help" ]; then
+    if [ ! -e ${FILE1} ]; then
+        echo "Downloading onnx file... save path: ${FILE1}"
+        curl https://storage.googleapis.com/ailia-models/${MODEL}/${FILE1} -o ${FILE1}
+    fi
+fi
+if [ ! "$1" = "-h" ] && [ ! "$1" = "--help" ]; then
+    if [ ! -e ${FILE2} ]; then
+        echo "Downloading onnx file... save path: ${FILE2}"
+        curl https://storage.googleapis.com/ailia-models/${MODEL}/${FILE2} -o ${FILE2}
+    fi
+    echo "ONNX file and Prototxt file are prepared!"
+fi
+if [ ! "$1" = "-h" ] && [ ! "$1" = "--help" ]; then
+    if [ ! -e ${FILE3} ]; then
+        echo "Downloading vocab file... save path: ${FILE3}"
+        curl https://storage.googleapis.com/ailia-models/${MODEL}/${FILE3} -o ${FILE3}
+    fi
+fi
+#execute
+./${MODEL} $*
