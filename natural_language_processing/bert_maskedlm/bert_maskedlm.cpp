76fa939f97a38ffb0790b063b448dfe4122c6228
Kazuki Kyakuno <kyakuno@axell.co.jp>
2024-07-16 18:28:52 +0900

    Remove cls and sep tokens

--- natural_language_processing/bert_maskedlm/bert_maskedlm.cpp
+++ natural_language_processing/bert_maskedlm/bert_maskedlm.cpp
@@ -4,7 +4,7 @@
 *      AILIA BERT maskedLM sample
 *    AUTHOR:
 *
-*    DATE:2023/06/14
+*    DATE:2024/07/16
 *
 *******************************************************************/
 
@@ -205,6 +205,10 @@ std::vector<int> encode(std::string text, struct AILIATokenizer *tokenizer){
 		setErrorDetail("ailiaTokenizerGetTokens", "");
 		return tokens;
 	}
+	const int CLS_TOKEN = 2;
+	const int SEP_TOKEN = 3;
+    tokens.erase(std::remove(tokens.begin(), tokens.end(), CLS_TOKEN), tokens.end());
+    tokens.erase(std::remove(tokens.begin(), tokens.end(), SEP_TOKEN), tokens.end());
 	return tokens;
 }
 
